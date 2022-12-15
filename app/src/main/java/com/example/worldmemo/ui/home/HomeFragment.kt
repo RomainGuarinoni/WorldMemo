79c1254b70168711dc9dad20b4ff0047478f19ed
RomainGuarinoni <romain.guar91@gmail.com>
2022-12-15 11:27:27 +0100

    feat: search view

--- app/src/main/java/com/example/worldmemo/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/worldmemo/ui/home/HomeFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.SearchView
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
@@ -23,21 +24,43 @@ class HomeFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
+
+        // Get the ViewModel to handle the Data of this fragment
         val homeViewModel =
             ViewModelProvider(this).get(HomeViewModel::class.java)
 
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
+        // Get the UI element
         val textView: TextView = binding.textHome
         val button:Button = binding.changeText
+        val searchView = binding.searchView
+
+        // Set the observer for the text
         homeViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
 
+        // Set the event listener on the button
         button.setOnClickListener{
             homeViewModel.changeText()
         }
+
+
+        // Remove auto focus of the searchView
+        searchView.clearFocus()
+        searchView.setOnQueryTextListener(object  : SearchView.OnQueryTextListener{
+            override fun onQueryTextSubmit(p0: String?): Boolean {
+                if(p0 != null) homeViewModel.changeText(p0)
+                return true
+            }
+
+            override fun onQueryTextChange(p0: String?): Boolean {
+                return true
+            }
+        })
+
 
         return root
     }
@@ -47,3 +70,5 @@ class HomeFragment : Fragment() {
         _binding = null
     }
 }
+
+
--- app/src/main/java/com/example/worldmemo/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/example/worldmemo/ui/home/HomeViewModel.kt
@@ -5,22 +5,25 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class HomeViewModel : ViewModel() {
-
     private val _text:MutableLiveData<String> = MutableLiveData("This is the home page")
 
- private val textArray:Array<String> = arrayOf("bonjour a tous", "cette app est incroyable", "je comprends rien a android")
- private var currentIndex = 0
+    private val textArray:Array<String> = arrayOf("bonjour a tous", "cette app est incroyable", "je comprends rien a android")
+    private var currentIndex = 0
 
- private fun incrementCurrentIndex(){
-     if(currentIndex == textArray.size -1){
-         currentIndex = 0
-     }else{
-         currentIndex++
-     }
- }
- fun changeText(){
-     incrementCurrentIndex()
-     _text.value = textArray[currentIndex]
- }
- val text: LiveData<String> = _text
+    private fun incrementCurrentIndex(){
+        if(currentIndex == textArray.size -1){
+             currentIndex = 0
+         }else{
+             currentIndex++
+         }
+    }
+    fun changeText(){
+        incrementCurrentIndex()
+        _text.value = textArray[currentIndex]
+    }
+
+    fun changeText(newText:String){
+        _text.value = newText
+    }
+    val text: LiveData<String> = _text
 }
