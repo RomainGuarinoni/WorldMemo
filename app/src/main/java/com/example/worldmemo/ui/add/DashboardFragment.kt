8cd6e6f1d732606b27927faf8a95508131cf391d
RomainGuarinoni <romain.guar91@gmail.com>
2022-12-21 00:19:36 +0100

    feat : create add fragment layout

--- app/src/main/java/com/example/worldmemo/ui/add/DashboardFragment.kt
+++ app/src/main/java/com/example/worldmemo/ui/add/DashboardFragment.kt
@@ -1,4 +1,4 @@
-package com.example.worldmemo.ui.dashboard
+package com.example.worldmemo.ui.add
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -7,11 +7,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.example.worldmemo.databinding.FragmentDashboardBinding
+import com.example.worldmemo.databinding.FragmentAddBinding
 
 class DashboardFragment : Fragment() {
 
-    private var _binding: FragmentDashboardBinding? = null
+    private var _binding: FragmentAddBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -22,16 +22,11 @@ class DashboardFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val dashboardViewModel =
-            ViewModelProvider(this).get(DashboardViewModel::class.java)
 
-        _binding = FragmentDashboardBinding.inflate(inflater, container, false)
+
+        _binding = FragmentAddBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textDashboard
-        dashboardViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
         return root
     }
 
